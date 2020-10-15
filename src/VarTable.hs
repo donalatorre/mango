72a4755d7e0c3c445e540f76b6ddfb15bf4cd3ce
freyes7 <fgrl1408@gmail.com>
2020-10-14 19:52:35 -0500

    new Types validation added

--- src/VarTable.hs
+++ src/VarTable.hs
@@ -2,19 +2,23 @@ module VarTable  where
 import DataTypes
 
 varTable :: Program -> [(String, Int)]
-varTable (Program typeDefList _ _ _ _) = typeDefVars typeDefList
+varTable (Program typeDefList _ _ _ _) = typeDefVars typeDefList (getTypes typeDefList)
 
-typeDefVars :: [TypeDef] -> [(String, Int)]
-typeDefVars [] = []
-typeDefVars  ((TypeDef _ typeSigList): t)= storeConstructors typeSigList ++ typeDefVars t
+getTypes :: [TypeDef] -> [String]
+getTypes [] = []
+getTypes ((TypeDef (newType, _) _): t) = (newType: getTypes t)
 
-storeConstructors :: [TypeSig] -> [(String, Int)]
-storeConstructors [] = []
-storeConstructors ((TypeConstr constructor typeSigList): t) = [(constructor, (countPrimitiveTypes typeSigList))] ++ storeConstructors t
+typeDefVars :: [TypeDef] -> [String] -> [(String, Int)]
+typeDefVars [] _ = []
+typeDefVars  ((TypeDef _ typeSigList): t) newTypes = storeConstructors typeSigList newTypes ++ typeDefVars t newTypes
 
-countPrimitiveTypes :: [TypeSig] -> Int
-countPrimitiveTypes [] = 0
-countPrimitiveTypes ((TypeConstr primitiveType _): t) = 
-	if elem primitiveType primitiveTypes
-		then 1 + countPrimitiveTypes t
+storeConstructors :: [TypeSig] -> [String] -> [(String, Int)]
+storeConstructors [] _= []
+storeConstructors ((TypeConstr constructor typeSigList): t) newTypes = [(constructor, (countPrimitiveTypes typeSigList newTypes))] ++ storeConstructors t newTypes
+
+countPrimitiveTypes :: [TypeSig] -> [String] -> Int
+countPrimitiveTypes [] _ = 0
+countPrimitiveTypes ((TypeConstr primitiveType _): t) newTypes= 
+	if elem primitiveType primitiveTypes || elem primitiveType newTypes
+		then 1 + countPrimitiveTypes t newTypes
 		else error ("Error, " ++ primitiveType ++ " is not a primitive type")
