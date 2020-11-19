bde2220c018225d9e21ba1e49df3e110997229d7
freyes7 <fgrl1408@gmail.com>
2020-11-18 21:13:28 -0600

    Pattern Matching

--- src/Eval.hs
+++ src/Eval.hs
@@ -5,11 +5,29 @@ import Data.Map
 data Data = DataInt Int | DataDouble Double | DataChar Char | DataBool Bool | DataConstr String [Data] deriving(Show)
 data MatchResult = NoMatch | Match (Map String Data) deriving(Show)
 
+instance Eq MatchResult where
+	(==) NoMatch NoMatch = True
+	(==) (Match e1) (Match e2) = True
+	(==) _ _ = False
+
+matchUnion :: [MatchResult] -> Map String Data
+matchUnion [] = fromList $ []
+matchUnion ((Match x): xl) = union x (matchUnion xl)
+
 patternMatch :: Data->Pattern->MatchResult
 patternMatch info (PatRef name) = Match $ fromList [(name, info)]
-patternMatch info (PatLit (PInt x)) = case info of
- (DataInt x) -> Match $ fromList []
- _ -> NoMatch
+patternMatch (DataInt x) (PatLit (PInt y)) = if x == y then Match $ fromList [] else NoMatch
+patternMatch (DataDouble x) (PatLit (PDouble y)) = if x == y then Match $ fromList [] else NoMatch
+patternMatch (DataChar x) (PatLit (PChar y)) = if x == y then Match $ fromList [] else NoMatch
+patternMatch (DataBool x) (PatLit (PBool y)) = if x == y then Match $ fromList [] else NoMatch
+patternMatch (DataConstr "List" (x:xl)) (PatConstr "Cons" [y, z]) = 
+	if ((patternMatch x y) == NoMatch || (patternMatch (DataConstr "List" xl) z) == NoMatch) then NoMatch
+		else (Match (matchUnion [(patternMatch x y), (patternMatch (DataConstr "List" xl) z)]))
+patternMatch (DataConstr x xl) (PatConstr y yl) = 
+	if (x == y && (length xl) == (length yl) && (notElem NoMatch (zipWith patternMatch xl yl)))
+		then Match (matchUnion (zipWith patternMatch xl yl)) else NoMatch
+patternMatch _ _ = NoMatch
+
 
 
 {--
